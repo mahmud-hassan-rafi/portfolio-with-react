2944c85ace09c85a22ddf7bd4c98c5c6ad3dce3f
Mahmud Hassan Rafi <mahmudhassanrafi12@gmail.com>
2025-11-28 04:30:26 +0600

    fixsome bugs

--- src/components/common/navbar/Navbar.jsx
+++ src/components/common/navbar/Navbar.jsx
@@ -6,7 +6,7 @@ import NavbarHireNowBtn from "./NavbarHireNowBtn";
 const Navbar = () => {
   return (
     <nav className="bg-[#e5e6e6] h-16 sm:h-17 md:h-18 lg:h-19 flex items-center w-full">
-      <div className="flex sm:justify-start md:justify-between items-center mx-5 w-full">
+      <div className="flex justify-between items-center mx-5 w-full">
         <NavbarLogo />
         <NavbarMenu />
         <NavbarHireNowBtn />
--- src/components/common/navbar/NavbarHireNowBtn.jsx
+++ src/components/common/navbar/NavbarHireNowBtn.jsx
@@ -3,7 +3,7 @@ import { FaBriefcase } from "react-icons/fa";
 
 const NavbarHireNowBtn = () => {
   return (
-    <div className="sm:w-1/5 md:w-1/4 flex items-center sm:justify-end">
+    <div className="sm:w-1/5 md:w-1/4 flex items-center justify-end">
       <button
         type="submit"
         className="text-[#0D9488] text-sm flex items-center gap-x-2 px-1 py-2 sm:p-2.5 md:px-3 cursor-pointer hover:bg-gray-300 transition duration-200 ease-in-out rounded-md font-bold border border-gray-300"
